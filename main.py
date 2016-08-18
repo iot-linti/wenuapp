c7320c8a17b9264bfc103477da22429b34c8a2e4
lfarkas <lfarkas@mail.linti.unlp.edu.ar>
2016-08-18 15:34:50 -0300

    mas temperaturas funcionando

--- main.py
+++ main.py
@@ -79,23 +79,6 @@ class Info(FloatLayout):
 				print "............."
 				line = GridLayout(cols=5, spacing=30)
 				line.bind(minimum_height=line.setter('height'))
-				datosactuales_control = self.procesar_datos_cocina(re[-1])
-				datosactuales_coc = self.procesar_datos_cocina(re[-2])
-
-
-				#~ for dato in range(len(self.etiquetas_coc)):
-					#~ self.ids[self.etiquetas_coc[dato]].text = datosactuales_coc[dato]
-					#~ self.ids[self.etiquetas_control[dato]].text = datosactuales_control[dato]
-
-				temp_color = self.calcular_color(re[-2]["temperature"])
-				self.ids["temperature_coc"].text = temp_color
-				self.ids["temperature_coc"].texture_update()
-
-				temp_color = self.calcular_color(re[-1]["temperature"])
-				self.ids["temperature"].text = temp_color
-				self.ids["temperature"].texture_update()
-
-
 				for r in re:
 					#~ print r
 					temp_color = self.calcular_color(r["temperature"]) #"[color=f10000]"+str(r["temperature"])+"[/color]" if r["temperature"] > dict_temp_amb["temperature"] + 5 else "[color=ffffff]"+str(r["temperature"])+"[/color]"
@@ -107,6 +90,68 @@ class Info(FloatLayout):
 					line.add_widget(Label(text=str(r["motion"])))
 
 				self.ids["scroll_grid"].add_widget(line)
+
+	def actualizar_mapa(self):
+		print "aslllllllllllllllllllllllllllllllllllllllllllllllll"
+		try:
+			#~ query = 'SELECT * as temperatura FROM climatizacion GROUP BY mote_id ORDER BY time desc LIMIT 1'
+			query = "SELECT * as temperatura FROM climatizacion WHERE mote_id = 'linti_cocina' ORDER BY time desc LIMIT 1"
+			query2 = "SELECT * as temperatura FROM climatizacion WHERE mote_id = 'linti_oficina_1' ORDER BY time desc LIMIT 1"
+			query3 = "SELECT * as temperatura FROM climatizacion WHERE mote_id = 'linti_control' ORDER BY time desc LIMIT 1"
+			query4 = "SELECT * as temperatura FROM climatizacion WHERE mote_id = 'linti_servidores' ORDER BY time desc LIMIT 1"
+			print query
+			print query2
+			print query3
+			print query4
+			res = self.client.query(query) #Consulta meramente de prueba
+			res2 = self.client.query(query2) #Consulta meramente de prueba
+			res3 = self.client.query(query3) #Consulta meramente de prueba
+			res4 = self.client.query(query4) #Consulta meramente de prueba
+		except:
+			print("Error al efectuar la consulta")
+		else:
+			print "sdfsdfdsfsfddsfsdfsdfdsfdsfdsfdsf"
+			print res
+			#~ self.ids["scroll_grid"].clear_widgets()
+			#cocina
+			for re in res:
+				#~ print "............."
+				#~ datosactuales_control = self.procesar_datos_cocina(re[-1])
+				#~ datosactuales_coc = self.procesar_datos_cocina(re[-2])
+
+
+				#~ for dato in range(len(self.etiquetas_coc)):
+					#~ self.ids[self.etiquetas_coc[dato]].text = datosactuales_coc[dato]
+					#~ self.ids[self.etiquetas_control[dato]].text = datosactuales_control[dato]
+
+				print "......--------------------------------"
+
+				print re
+				print "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+				temp_color = self.calcular_color(re[-1]["temperature"])
+				self.ids["temperature_coc"].text = temp_color
+				self.ids["temperature_coc"].texture_update()
+
+			#oficina
+			for re in res2:
+				temp_color = self.calcular_color(re[-1]["temperature"])
+				self.ids["temperature"].text = temp_color
+				self.ids["temperature"].texture_update()
+
+			#control
+			for re in res3:
+				temp_color = self.calcular_color(re[-1]["temperature"])
+				self.ids["temperature_cont"].text = temp_color
+				self.ids["temperature_cont"].texture_update()
+
+			for re in res4:
+				temp_color = self.calcular_color(re[-1]["temperature"])
+				self.ids["temperature_serv"].text = temp_color
+				self.ids["temperature_serv"].texture_update()
+
+
+				#~ self.ids["scroll_grid"].add_widget(line)
 
 	def iniciar(self, actual_screen, next_screen):
 		Logger.info('datos: cambio pantalla')
@@ -120,6 +165,7 @@ class Info(FloatLayout):
 		else:
 			self.onNextScreen(actual_screen, next_screen)
 		self.actualizar()
+		self.actualizar_mapa()
 
 	def onBackBtn(self):
 		# Check if there are any screens to go back to
