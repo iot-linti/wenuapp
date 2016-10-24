73b92d67ac32006f7c2e6076bcd2afc0fa6a1899
lfarkas <lfarkas@mail.linti.unlp.edu.ar>
2016-10-24 14:08:07 -0300

    servicio funcionando

--- service/main.py
+++ service/main.py
@@ -27,25 +27,23 @@ def leer_datos_motas(cli, ctrl):
 	try:
 		for mid in motas_ids:
 			for m_id in mid:
-				print m_id
-				print m_id[1]
-				mota = cli.query("SELECT * FROM medicion WHERE mota_id = '"+m_id[1]+"' ORDER BY time desc LIMIT 1")
-				if (mota.items()[0][1].next()['temeratura'] > (control['temperatura'] + 5)):
-					notification.notify('InfluxDB', mota['mota_id']+' esta overheating -'+mota['temperatura']+'C')
-				else:
-					notification.notify('InfluxDB', m_id['mota_id']+' todo OK -'+mota['temperature']+'C')
+				mota = cli.query("SELECT * FROM medicion WHERE mota_id = '"+m_id["mota_id"]+"' ORDER BY time desc LIMIT 1")
+				m = mota.items()[0][1].next()
+				if (m['temperatura'] > (control['temperatura'] + 5)):
+					notification.notify('InfluxDB', m['mota_id']+' esta overheating - '+str(m['temperatura'])+'C')
+				#~ else:
+					#~ notification.notify('InfluxDB', mota.items()[0][1].next()['mota_id']+' todo OK - '+str(mota.items()[0][1].next()['temperatura'])+'C')
 		#~ notification.notify('influxdb', 'anda semi-bien')
 	except Exception as e:
 		print e
-		notification.notify('influxError','aksdjlkasjdaskdjksd'+str(e))
-	notification.notify('llama','leer')
+		notification.notify('influxError',str(e))
+	#~ notification.notify('llama','leer')
 
 
 if __name__ == '__main__':
 	con = conexion()
 	print con
 	if con:
-		print "entro"
 		while True:
 			leer_datos_motas(con, 'linti_control')
 			time.sleep(1)
